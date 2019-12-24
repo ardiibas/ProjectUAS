55b4a3aca95d27eafbd827cda10f04955f1a7bc3
Ardi Baskoro <ardi@gamatechno.com>
2019-12-24 11:29:17 +0700

    redesign add barang

--- app/src/main/java/app/example/projectuas/activity/AddBarang.kt
+++ app/src/main/java/app/example/projectuas/activity/AddBarang.kt
@@ -7,11 +7,15 @@ import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
 import app.example.projectuas.R
 import kotlinx.android.synthetic.main.add_barang.*
+import kotlinx.android.synthetic.main.toolbar_white.*
 
 class AddBarang : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_barang)
+
+        initToolbar();
+
         bt_Next.setOnClickListener {
             val gotoResult = Intent()
             if (TextUtils.isEmpty(jT_nama.text) ||
@@ -20,10 +24,10 @@ class AddBarang : AppCompatActivity() {
                     TextUtils.isEmpty(jTHarga.text)) {
                 setResult(Activity.RESULT_CANCELED, gotoResult)
             } else {
-                val nama    = jT_nama.text.toString()
-                val merk    = jT_merk.text.toString()
-                val qty     = jT_Qty.text.toString()
-                val harga   = jTHarga.text.toString()
+                val nama = jT_nama.text.toString()
+                val merk = jT_merk.text.toString()
+                val qty = jT_Qty.text.toString()
+                val harga = jTHarga.text.toString()
 
                 gotoResult.putExtra(EXTRA_REPLY_NAME, nama)
                 gotoResult.putExtra(EXTRA_REPLY_MERK, merk)
@@ -33,7 +37,11 @@ class AddBarang : AppCompatActivity() {
             }
             finish()
         }
-        backToHome1.setOnClickListener {
+    }
+
+    private fun initToolbar() {
+        toolbar.setNavigationIcon(R.drawable.ic_house);
+        toolbar.setNavigationOnClickListener {
             val backtoHome = Intent(this, MainActivity::class.java)
             startActivity(backtoHome)
             finish()
@@ -41,9 +49,9 @@ class AddBarang : AppCompatActivity() {
     }
 
     companion object {
-        const val EXTRA_REPLY_NAME  = "com.example.android.wordlistsql.REPLY_NAME"
-        const val EXTRA_REPLY_MERK  = "com.example.android.wordlistsql.REPLY_MERK"
-        const val EXTRA_REPLY_QTY   = "com.example.android.wordlistsql.REPLY_QTY"
+        const val EXTRA_REPLY_NAME = "com.example.android.wordlistsql.REPLY_NAME"
+        const val EXTRA_REPLY_MERK = "com.example.android.wordlistsql.REPLY_MERK"
+        const val EXTRA_REPLY_QTY = "com.example.android.wordlistsql.REPLY_QTY"
         const val EXTRA_REPLY_HARGA = "com.example.android.wordlistsql.REPLY_HARGA"
     }
 }
